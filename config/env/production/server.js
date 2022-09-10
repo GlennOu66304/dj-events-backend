702718532bfda6eec88bf91374befe140fa400cf
Glenn <glenou@yahoo.com>
2022-09-11 02:25:54 +0800

    test

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 // Path: ./config/env/production/server.js`
-
 module.exports = ({ env }) => ({
   url: env('MY_HEROKU_URL'),
-  proxy: true,
-  app: {
-      keys: env.array("APP_KEYS", ["DATABASE_URL", "HEROKU_POSTGRESQL_MAROON_URL"]),
-  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  }
 });
