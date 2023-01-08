6f034e2b08ae5ec58a994fb3c5242f7fc2361c68
Glenn <2738328919@qq.com>
2023-01-08 23:26:48 +0400

    migrate the project from heroku to render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,12 @@
 // Path: ./config/env/production/server.js`
+// module.exports = ({ env }) => ({
+//   url: env('MY_HEROKU_URL'),
+//   admin: {
+//     auth: {
+//       secret: env("jwtSecret"),
+//     },
+//   }
+// });
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
-  admin: {
-    auth: {
-      secret: env("jwtSecret"),
-    },
-  }
+  url: env("RENDER_EXTERNAL_URL"),
 });
--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',["testKey1", "testKey2"]),
+    // How do I solve "Middleware "strapi::session": App keys are required" error in my deployment of Strapi to Heroku?
+    // https://stackoverflow.com/questions/70930881/how-do-i-solve-middleware-strapisession-app-keys-are-required-error-in-my
   },
 });
